52657a2c584e8adde138e00b4fda05639b8dd6ab
Oleg <loytloloyu@gmail.com>
2021-03-15 20:41:29 +0200

    f

--- src/main/java/org/example/MyServlet.java
+++ src/main/java/org/example/MyServlet.java
@@ -5,6 +5,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.example.entity.Applications;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyServlet extends JsonServlet {
 
@@ -12,17 +14,30 @@ public class MyServlet extends JsonServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-
-        Applications applications =new Applications()
+        Applications applicat1 = new Applications()
                 .setName("Протокол связи")
                 .setDescription("Протокол описывающий связь")
                 .setDate("14.01.2020");
 
-        writeJson(applications,resp);
+        Applications applicat2 = new Applications()
+                .setName("Протокол коммуникации")
+                .setDescription("Протокол описывающий коммуникации")
+                .setDate("14.01.2021");
+
+
+        Map<String, Applications> applicationsMap = new HashMap<>();
+        applicationsMap.put("1", applicat1);
+        applicationsMap.put("2", applicat2);
+
+
+        String id = req.getParameter("id");
+        if (id != null && applicationsMap.containsKey(id)) {
+            Applications newapplications = applicationsMap.get(id);
+            writeJson(newapplications, resp);
+        }
 
 
     }
-
 
 
 }
